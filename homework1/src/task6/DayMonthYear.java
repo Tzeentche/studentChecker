5620c419b7726c62672e6a0d4c853325ee7c70c3
Tzeentche <ilyasugako87@gmail.com>
2020-02-27 23:27:59 +0300

    Correct task with days.

--- homework1/src/task6/DayMonthYear.java
+++ homework1/src/task6/DayMonthYear.java
@@ -2,19 +2,41 @@ package task6;
 
 //      Имеются три числа - день, месяц, год. Вывести в виде трех чисел дату следующего дня.
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DayMonthYear {
 
     public static void main(String[] args) {
 
-        int day = 0, month = 0, year = 0;
+        int[] days = new int[3];
+        String[] names = {"DD", "MM", "YY"};
 
         Scanner scan = new Scanner(System.in);
+
+        try {
+
+            for (int i = 0; i < days.length; i++) {
+                System.out.println("Please, enter day, month and year at format " + names[i]);
+                days[i] = scan.nextInt();
+            }
+            scan.close();
+
+        } catch(InputMismatchException ex) {
+
+            System.out.println("You did something wrong. Try again!");
+        }
+
+        System.out.println("Next day date is " + userInput(days));
     }
 
-    static int userInput(int date) {
+    static String userInput(int[] date) {
 
+        String tomorrow = "";
 
+        for (int i = 0; i < date.length; i++) {
+            tomorrow += Integer.toString(date[i]) + ":";
+        }
+        return tomorrow;
     }
 }
