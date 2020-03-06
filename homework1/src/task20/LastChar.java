0c9e7ff0db41840bf3c15453cfca8b580cb54328
Tzeentche <ilyasugako87@gmail.com>
2020-03-06 19:59:21 +0300

    6-th task was done.

---
+++ homework1/src/task20/LastChar.java
@@ -0,0 +1,10 @@
+package task20;
+
+//      Имеется строка с текстом. Вывести текст, составленный из последних букв всех слов.
+
+public class LastChar {
+
+    public static void main(String[] args) {
+
+    }
+}
--- homework1/src/task6/DayMonthYear.java
+++ homework1/src/task6/DayMonthYear.java
@@ -2,41 +2,130 @@ package task6;
 
 //      Имеются три числа - день, месяц, год. Вывести в виде трех чисел дату следующего дня.
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
 
 public class DayMonthYear {
 
+    static int[] days = new int[3];
+    static String[] names = {"Day", "Month", "Year"};
+
     public static void main(String[] args) {
 
-        int[] days = new int[3];
-        String[] names = {"DD", "MM", "YY"};
+        DayMonthYear daMoYe = new DayMonthYear();
+        daMoYe.userInput();
+    }
+
+    static void userInput() {
 
         Scanner scan = new Scanner(System.in);
 
         try {
 
             for (int i = 0; i < days.length; i++) {
-                System.out.println("Please, enter day, month and year at format " + names[i]);
+                System.out.println("Please, enter " + names[i] + " at format [D:M:Y]:");
                 days[i] = scan.nextInt();
             }
             scan.close();
+
+            if((days[0] > 31) | (days[0]) <= 0) {
+                    System.out.println("Date is impossible! Please, try again and enter correct date!");
+                    System.exit(0);
+            }
+
+            if((days[1] > 13) | (days[1]) <= 0) {
+                    System.out.println("Month is impossible! Please, try again and enter correct moth!");
+                    System.exit(0);
+
+            } else if ((days[2] < 0) | (days[2] > 99)) {
+                    System.out.println("Year is impossible! Please, try again and enter correct year!");
+                    System.exit(0);
+                }
 
         } catch(InputMismatchException ex) {
 
             System.out.println("You did something wrong. Try again!");
         }
 
-        System.out.println("Next day date is " + userInput(days));
+        System.out.println("Next day date is " + userOutput(days));
     }
 
-    static String userInput(int[] date) {
+    static String userOutput(int[] date) {
 
         String tomorrow = "";
+        int[] check = revisor(date);
 
-        for (int i = 0; i < date.length; i++) {
-            tomorrow += Integer.toString(date[i]) + ":";
+        if ((check[1] == 4 |
+                check[1] == 6 |
+                check[1] == 9 |
+                check[1] == 11) && (check[0] == 30)) {
+
+            check[1] += 1;
+            check[0] = 1;
+
+        } else if ((check[1] == 1 |
+                check[1] == 3 |
+                check[1] == 5 |
+                check[1] == 7 |
+                check[1] == 8 |
+                check[1] == 10) && (check[0] == 31)) {
+            check[1] += 1;
+            check[0] = 1;
+
+        } else if((check[1] == 12) && check[0] == 31) {
+                check[2] += 1;
+
+                if (check[2] == 100) {
+
+                    check[2] = 00;
+                }
+                check[1] = 1;
+                check[0] = 1;
+
+        } else if ((check[2] % 4 != 0) && (check[1] == 2) && (check[0] == 28) || (check[2] % 4 == 0) && (check[1] == 2) && (check[0] == 29)) {
+            check[1] += 1;
+            check[0] = 1;
+
+        } else {
+
+            check[0] += 1;
         }
+
+            for (int i = 0; i < check.length; i++) {
+
+                if (check[i] < 10) {
+
+                    tomorrow += "0";
+                }
+
+                if (i < (check.length - 1)) {
+
+                    tomorrow += check[i] + ":";
+                } else {
+                    tomorrow += check[i];
+                }
+            }
+
         return tomorrow;
+    }
+
+    static int[] revisor(int[] date) {
+
+        int[] today = new int[days.length];
+
+        if ((date[2] % 4 != 0) && (date[1] == 2) && (date[0] > 28) || (date[2] % 4 == 0) && (date[1] == 2) && (date[0] > 29)) {
+            System.out.println("Date is impossible! Please, try again and enter correct date!");
+            System.exit(0);
+
+        } else if ((date[1] == 4 | date[1] == 6 | date[1] == 9 | date[1] == 11) && (date[0] > 30)) {
+            System.out.println("Date is impossible! Please, try again and enter correct date!");
+            System.exit(0);
+
+        } else {
+
+            for (int i = 0; i < days.length; i++) {
+                today[i] = date[i];
+            }
+        }
+        return today;
     }
 }
