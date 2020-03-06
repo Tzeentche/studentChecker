56d5dfa8dd59764eb1cd3781935098e46f8ac368
Tzeentche <ilyasugako87@gmail.com>
2020-03-06 20:00:06 +0300

    19-th task was added.

---
+++ homework1/src/task21/Performance.java
@@ -0,0 +1,11 @@
+package task21;
+
+//      Напишите два цикла, выполняющих многократное сложение строк, один с помощью оператора сложения и String, а другой с помощью
+//      StringBuilder и метода append. Сравните скорость их выполнения.
+
+public class Performance {
+
+    public static void main(String[] args) {
+
+    }
+}
