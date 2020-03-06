191b0d62e30b865adebad1e7abf33b3e207e8b39
Tzeentche <ilyasugako87@gmail.com>
2020-03-06 19:50:43 +0300

    Classwork 6. Create MyEnum.

---
+++ ITAcademyJAVA/src/by/sugako/toEat/MyEnum.java
@@ -0,0 +1,16 @@
+package by.sugako.toEat;
+
+public enum MyEnum {
+
+    BREAFAST(new Menu("всянка, яйца жаренные, кофе, фрукты")),
+    LUNCH(new Menu()),
+    DINNER(new Menu()),
+    LATE_DINNER(new Menu());
+
+    private Menu menu;
+
+    MyEnum(Menu menu) {
+
+        this.menu = menu;
+    }
+}
