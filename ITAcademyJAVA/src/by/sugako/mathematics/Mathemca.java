d36dbb5b0b4c9396137a214f4cbbc4c40b5b9b75
Tzeentche <ilyasugako87@gmail.com>
2020-03-06 19:49:25 +0300

    Classwork 4.

---
+++ ITAcademyJAVA/src/by/sugako/mathematics/Mathemca.java
@@ -0,0 +1,15 @@
+package by.sugako.mathematics;
+
+import java.lang.*;
+
+public class Mathemca {
+
+    public static void main(String[] args) {
+
+        double rDouble = Math.random();
+        System.out.println(Math.sqrt(900000000));
+
+        System.out.println(Math.pow(2, 8));
+
+    }
+}
