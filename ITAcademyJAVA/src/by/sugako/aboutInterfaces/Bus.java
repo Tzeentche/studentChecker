f4bf7e1af98d33a9f2afa3375c7525c2f4608aeb
Tzeentche <ilyasugako87@gmail.com>
2020-03-10 11:18:52 +0300

    Developed class Bus.

---
+++ ITAcademyJAVA/src/by/sugako/aboutInterfaces/Bus.java
@@ -0,0 +1,16 @@
+package by.sugako.aboutInterfaces;
+
+public class Bus extends Vehicle implements Movable, Payable {
+
+    public void move() {
+        System.out.println("Bus is moving.");
+    }
+
+    public void pay() {
+        System.out.println("Pay 0.70 BYN.");
+    }
+
+    void drive() {
+        System.out.println("Driving bus.");
+    }
+}
