8c66187f882350d03f56c1f43075e7d1e164f4d8
Tzeentche <ilyasugako87@gmail.com>
2020-03-10 12:04:29 +0300

    Create class Uber.

---
+++ ITAcademyJAVA/src/by/sugako/aboutInterfaces/Uber.java
@@ -0,0 +1,27 @@
+package by.sugako.aboutInterfaces;
+
+public class Uber extends Vehicle implements Movable, Payable {
+
+    public void move() {
+        System.out.println("Uber is moving.");
+    }
+
+    public void pay() {
+        System.out.println("Pay 7.00 BYN.");
+    }
+
+    public void move(int i) {
+        System.out.println("Uber is moving " + i + " km.");
+    }
+
+    public void drive() {
+        System.out.println("Driving Uber.");
+        getLicensePlateNumber();
+        super.getLicensePlateNumber();
+    }
+
+    public String getLicensePlateNumber() {
+
+        return super.getLicensePlateNumber() + "UBER";
+    }
+}
