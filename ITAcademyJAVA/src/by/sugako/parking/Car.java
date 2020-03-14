f00d4c03cd5635f936848e3242baa4bd8a757af7
Tzeentche <ilyasugako87@gmail.com>
2020-03-14 19:59:42 +0300

    Add class Car.

---
+++ ITAcademyJAVA/src/by/sugako/parking/Car.java
@@ -0,0 +1,16 @@
+package by.sugako.parking;
+
+public class Car {
+
+    private String plateNumber;
+
+    public Car(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+
+}
