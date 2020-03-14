59abec6e8db7a402822fb90dca7abb02152b25d7
Tzeentche <ilyasugako87@gmail.com>
2020-03-14 20:03:37 +0300

    Add class My Annotated Class.

---
+++ ITAcademyJAVA/src/by/sugako/lesson13/MyAnnotatedClass.java
@@ -0,0 +1,24 @@
+//package by.sugako.lesson13;
+//
+//public class MyAnnotatedClass {
+//
+//    private String className;
+//
+//    @MyFirstAnnotation(version = 2, description = "getter")
+//    public MyAnnotatedClass(String className) {
+//        this.className = className;
+//    }
+//
+//    @MyFirstAnnotation(description = "getter", version = 1)
+//    public String getClassName() {
+//        return className;
+//    }
+//
+//    public MyAnnotatedClass(String className) {
+//        this.className = className;
+//    }
+//
+//    public String getClassName() {
+//        return className;
+//    }
+//}
