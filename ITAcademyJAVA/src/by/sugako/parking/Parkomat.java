4e9b79198501b1fcd717421b9dca87b194dd7d29
Tzeentche <ilyasugako87@gmail.com>
2020-03-14 20:02:16 +0300

    Add class Parkomat.

---
+++ ITAcademyJAVA/src/by/sugako/parking/Parkomat.java
@@ -0,0 +1,39 @@
+package by.sugako.parking;
+
+import java.util.Scanner;
+
+public class Parkomat {
+
+    private Scanner scaner;
+
+    public void helloMessage(String map) {
+        System.out.println(map);
+        System.out.println("Hello. Please, enter plate number!");
+    }
+
+    public String readPlateNumber() {
+        while(true) {
+            System.out.println("Plate number: ");
+            if(scaner == null) {
+                scaner = new Scanner(System.in);
+            }
+
+            String newPlateNumber = scaner.nextLine();
+            if(newPlateNumber != null && !"".equals(newPlateNumber.trim())) {
+                return newPlateNumber;
+            }
+        }
+    }
+
+    public void showParkingFull() {
+        System.out.println("There are no free places!");
+    }
+
+    public void showParkedPlace(int placeNumber) {
+        System.out.println("Successfully parked on place " + placeNumber);
+    }
+
+    public void showGoodBye(String plateNumber) {
+        System.out.println("Good bye, car " + plateNumber);
+    }
+}
