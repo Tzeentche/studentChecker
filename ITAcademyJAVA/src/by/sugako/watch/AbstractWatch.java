9f9de2c7e276ab92fbf5ec98df0680690eb45cb3
Tzeentche <ilyasugako87@gmail.com>
2020-03-14 19:47:41 +0300

    Create abstract watch class.

---
+++ ITAcademyJAVA/src/by/sugako/watch/AbstractWatch.java
@@ -0,0 +1,32 @@
+package by.sugako.watch;
+
+import java.util.*;
+
+public abstract class AbstractWatch {
+
+    private double price;
+
+    public AbstractWatch(){
+        System.out.println("Construct Abstract watch");
+    }
+
+    public AbstractWatch(double price) {
+        this.price = price;
+    }
+
+    public void showTime() {
+        System.out.println(new Date());
+    }
+
+    void printColor(){
+        System.out.println("Print Abstract watchs color");
+    }
+
+    void printSize() {
+        System.out.println("XXL");
+    }
+
+    void printPrice() {
+        System.out.println("1000 EUR");
+    }
+}
