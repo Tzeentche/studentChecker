39a3c13ebbe4c09a042d436ddada21def9c727ea
Tzeentche <ilyasugako87@gmail.com>
2020-03-14 19:54:07 +0300

    Add Test My Enum class.

--- ITAcademyJAVA/src/by/sugako/toEat/TestMyEnum.java
+++ ITAcademyJAVA/src/by/sugako/toEat/TestMyEnum.java
@@ -7,10 +7,10 @@ public class TestMyEnum {
     public static void main(String[] args) {
 
         MyEnum myEnum1 = MyEnum.BREAFAST;
-        System.out.println(myEnum1 + " " + myEnum1.ordinal() + " " + myEnum1.getMenu());
+//        System.out.println(myEnum1 + " " + myEnum1.ordinal() + " " + myEnum1.getMenu());
 
         MyEnum myEnum2 = MyEnum.valueOf("DINNER");
-        System.out.println(myEnum2 + " " + myEnum2.ordinal() + " " + myEnum2.getMenu());
+//        System.out.println(myEnum2 + " " + myEnum2.ordinal() + " " + myEnum2.getMenu());
 
         System.out.println(Arrays.toString(MyEnum.values()));
     }
