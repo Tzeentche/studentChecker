fd55cfc6c4c4dcc02331cf2558d134fb57cdeaa9
Tzeentche <ilyasugako87@gmail.com>
2020-03-14 20:02:50 +0300

    Add class Main.

---
+++ ITAcademyJAVA/src/by/sugako/parking/Main.java
@@ -0,0 +1,11 @@
+package by.sugako.parking;
+
+public class Main {
+
+    static ParkingManager parkingManager = new ParkingManager();
+
+    public static void main(String[] args) {
+
+    parkingManager.startParking();
+    }
+}
