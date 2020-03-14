ad6f830540fb8163b952ec8ada33a7719c894724
Tzeentche <ilyasugako87@gmail.com>
2020-03-14 19:51:46 +0300

    Add main class.

---
+++ ITAcademyJAVA/src/by/sugako/watch/MainClass.java
@@ -0,0 +1,25 @@
+package by.sugako.watch;
+
+import by.sugako.watch.digitalWatch.DigitalWatch;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        AbstractWatch watch = new MechanicalWatcch(155.99);
+        watch.showTime();
+        watch.printColor();
+        watch.printSize();
+
+        AbstractWatch watch1 = new QuartzWatch();
+        watch1.showTime();
+
+        AbstractWatch watch2 = new DigitalWatch();
+        watch2.showTime();
+        watch2.printColor();
+        watch2.printSize();
+
+        AbstractWatch watch3 = new NewQuartzWatch((MechanicalWatcch)watch);
+        watch3.printColor();
+        watch3.showTime();
+    }
+}
