90468ca1923e332c8196c133fcbcdd9eb752f6da
Tzeentche <ilyasugako87@gmail.com>
2020-03-19 23:36:01 +0300

    Add Repka class to FairyTale.

---
+++ ITAcademyJAVA/src/by/sugako/fairyTale/Repka.java
@@ -0,0 +1,16 @@
+package by.sugako.fairyTale;
+
+public class Repka {
+
+    private int weight;
+
+    public void grow(int kilo) {
+        weight = kilo;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+
+}
