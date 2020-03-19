5cc6dca057fcf2efdf3d13b458acf14d9905e179
Tzeentche <ilyasugako87@gmail.com>
2020-03-19 23:35:12 +0300

    Add Zhuchka class to FairyTale.

---
+++ ITAcademyJAVA/src/by/sugako/fairyTale/Zhuchka.java
@@ -0,0 +1,16 @@
+package by.sugako.fairyTale;
+
+public class Zhuchka {
+
+    public static final int ZHUCHKA_SHARE = 10;
+
+    public Repka pullRepka(Repka repka, int leftWeight) throws VeryBigRepkaExceeption {
+        if(leftWeight > ZHUCHKA_SHARE) {
+
+            Koshka koshka = new Koshka();
+            return koshka.pullRepka(repka, leftWeight - ZHUCHKA_SHARE);
+        } else {
+            return repka;
+        }
+    }
+}
