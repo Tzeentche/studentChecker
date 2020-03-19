4fac70bab98229fbdaeadc3b2a1e625523892840
Tzeentche <ilyasugako87@gmail.com>
2020-03-19 23:34:31 +0300

    Add Babka class to FairyTale.

---
+++ ITAcademyJAVA/src/by/sugako/fairyTale/Babka.java
@@ -0,0 +1,16 @@
+package by.sugako.fairyTale;
+
+public class Babka {
+
+    public static final int BABKA_SHARE = 20;
+
+    public Repka pullRepka(Repka repka, int leftWeight) throws VeryBigRepkaExceeption {
+        if(leftWeight > BABKA_SHARE) {
+//            NEED HEEEELP!!!!
+            Vnuchka vnuchka = new Vnuchka();
+            return vnuchka.pullRepka(repka, leftWeight - BABKA_SHARE);
+        } else {
+            return repka;
+        }
+    }
+}
