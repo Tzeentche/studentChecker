a9ccc013a99d4824e1db95ce970d8085ed566a73
Tzeentche <ilyasugako87@gmail.com>
2020-03-22 00:27:23 +0300

    Refactored code in class Car.

--- ITAcademyJAVA/src/by/sugako/lesson11/aboutInterfaces/Car.java
+++ ITAcademyJAVA/src/by/sugako/lesson11/aboutInterfaces/Car.java
@@ -9,4 +9,9 @@ public class Car extends Vehicle implements Movable {
     public void drive() {
         System.out.println("Driving Car.");
     }
+
+    @Override
+    public void pay() {
+
+    }
 }
