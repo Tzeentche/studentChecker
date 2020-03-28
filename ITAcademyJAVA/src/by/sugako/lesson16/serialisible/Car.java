9d69ce4fd8b9e8cade9a81a6e19d037ffc1c00d3
Tzeentche <ilyasugako87@gmail.com>
2020-03-28 22:48:23 +0300

    Refactored code in class Car.

--- ITAcademyJAVA/src/by/sugako/lesson16/serialisible/Car.java
+++ ITAcademyJAVA/src/by/sugako/lesson16/serialisible/Car.java
@@ -4,11 +4,16 @@ import java.io.Serializable;
 
 public class Car implements Serializable {
 
-    static
+    public static String type = "auto";
     private String maker;
-    private String model;
+    transient private String model;
     private short year;
-    private double price;
+    private Double price;
+    private Float discount;
+
+    public Car() {
+        System.out.println("Call Car constructor");
+    }
 
     public String getMaker() {
         return maker;
@@ -34,12 +39,51 @@ public class Car implements Serializable {
         this.year = year;
     }
 
-    public double getPrice() {
+    public Double getPrice() {
         return price;
     }
 
-    public void setPrice(double price) {
+    public void setPrice(Double price) {
         this.price = price;
     }
 
+    public Float getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Float discount) {
+        this.discount = discount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Car car = (Car) o;
+
+        if (year != car.year) return false;
+        if (maker != null ? !maker.equals(car.maker) : car.maker != null) return false;
+        if (model != null ? !model.equals(car.model) : car.model != null) return false;
+        return price != null ? price.equals(car.price) : car.price == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = maker != null ? maker.hashCode() : 0;
+        result = 31 * result + (model != null ? model.hashCode() : 0);
+        result = 31 * result + (int) year;
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "maker='" + maker + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                '}';
+    }
 }
