e9e729c64340eb6788fee2b5a801511891128e3a
Tzeentche <ilyasugako87@gmail.com>
2020-03-28 22:50:16 +0300

    Refactored code in class ReadFromFile.

--- ITAcademyJAVA/src/by/sugako/lesson16/serialisible/ReadFromFile.java
+++ ITAcademyJAVA/src/by/sugako/lesson16/serialisible/ReadFromFile.java
@@ -1,4 +1,26 @@
 package by.sugako.lesson16.serialisible;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class ReadFromFile {
+
+    public String readFromFile(String fileName) {
+        try (FileInputStream fis = new FileInputStream(fileName)) {
+            System.out.println("Available: " + fis.available());
+            byte[] bytes = new byte[fis.available()];
+            int bytesCount = fis.read(bytes);
+            System.out.println("Read: " + bytesCount);
+            return new String(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        ReadFromFile readFromFile = new ReadFromFile();
+        String data = readFromFile.readFromFile("readme.txt");
+        System.out.println("Data=" + data);
+    }
 }
