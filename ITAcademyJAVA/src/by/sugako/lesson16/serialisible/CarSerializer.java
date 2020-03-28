71f168480b502fcdeacf515ba86c883e64a4ae84
Tzeentche <ilyasugako87@gmail.com>
2020-03-28 22:48:45 +0300

    Refactored code in class CarSerializer.

--- ITAcademyJAVA/src/by/sugako/lesson16/serialisible/CarSerializer.java
+++ ITAcademyJAVA/src/by/sugako/lesson16/serialisible/CarSerializer.java
@@ -1,4 +1,31 @@
 package by.sugako.lesson16.serialisible;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class CarSerializer {
+
+    public void serialize(Car car) {
+        try (FileOutputStream fos = new FileOutputStream("car.data");
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(car);
+            oos.flush();
+            fos.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.setMaker("Volvo");
+        car.setModel("XC60");
+        car.setYear((short) 2020);
+        car.setPrice(200000.99);
+        System.out.println(car);
+        System.out.println(car.type);
+
+        new CarSerializer().serialize(car);
+    }
 }
