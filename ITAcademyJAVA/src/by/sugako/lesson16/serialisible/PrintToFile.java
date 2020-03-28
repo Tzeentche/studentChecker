e23560a1b9a0bb9e4c7678e04c38733b1d7552cb
Tzeentche <ilyasugako87@gmail.com>
2020-03-28 22:49:58 +0300

    Refactored code in class PrintToFile.

--- ITAcademyJAVA/src/by/sugako/lesson16/serialisible/PrintToFile.java
+++ ITAcademyJAVA/src/by/sugako/lesson16/serialisible/PrintToFile.java
@@ -1,4 +1,26 @@
 package by.sugako.lesson16.serialisible;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class PrintToFile {
+
+    public boolean printToFile(String data, String fileName) {
+
+        try (FileOutputStream fos = new FileOutputStream(fileName)) {
+            fos.write(data.getBytes());
+            fos.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        boolean result = new PrintToFile().printToFile("Hello Java 2", "readme.txt");
+        if (result) System.out.println("Finished");
+        else System.out.println("Error");
+    }
 }
