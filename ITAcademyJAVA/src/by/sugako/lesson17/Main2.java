65cba5950c5ad5fc36dab748ba22fef6395e0892
Tzeentche <ilyasugako87@gmail.com>
2020-03-31 00:37:39 +0300

    Add class Main2 to lesson 17.

---
+++ ITAcademyJAVA/src/by/sugako/lesson17/Main2.java
@@ -0,0 +1,20 @@
+package by.sugako.lesson17;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        new Thread(new PersonThread(), "Man").start();
+        new Thread(new PersonThread(), "Woman").start();
+    }
+
+}
+
+class PersonThread implements Runnable {
+
+    @Override
+    public void run() {
+        Room room = Room.getInstance();
+        room.occupy();
+        room.release();
+    }
+}
