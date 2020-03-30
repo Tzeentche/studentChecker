a690b657026a539f94ea2934bc3af64cffd14e2b
Tzeentche <ilyasugako87@gmail.com>
2020-03-30 23:58:28 +0300

    Add class Parking.Add class BankomatImpl to lesson 17.

---
+++ ITAcademyJAVA/src/by/sugako/lesson17/bankomat/BankomatImpl.java
@@ -0,0 +1,48 @@
+package by.sugako.lesson17.bankomat;
+
+import by.sugako.lesson17.client.NoSuchAmountException;
+
+public class BankomatImpl implements Bankomat {
+
+    private final Cash money = new Cash();
+    private boolean isOpen = true;
+
+    @Override
+    public Integer get(int targetAmount) {
+        synchronized (money) {
+            if (targetAmount > money.getAmount()) {
+                try {
+                    money.wait(7_000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            money.minus(targetAmount);
+        }
+        return targetAmount;
+    }
+
+    @Override
+    public void add(Integer amount) {
+        synchronized (money){
+            money.plus(amount);
+            money.notifyAll();
+        }
+    }
+
+    @Override
+    public void printBalance() {
+        synchronized (money) {
+            System.out.println("In bankomat " + money + ".00 BYN");
+        }
+    }
+
+    @Override
+    public void close() {
+        isOpen = false;
+    }
+
+    public boolean isOpen() {
+        return isOpen;
+    }
+}
