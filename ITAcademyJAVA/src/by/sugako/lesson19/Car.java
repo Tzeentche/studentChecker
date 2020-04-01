d53eb31fed6560bdefd955d28028761b106a8c4f
Tzeentche <ilyasugako87@gmail.com>
2020-04-01 23:40:09 +0300

    Add class Car to lesson 19.

---
+++ ITAcademyJAVA/src/by/sugako/lesson19/Car.java
@@ -0,0 +1,20 @@
+package by.sugako.lesson19;
+
+public abstract class Car {
+
+    private CarType model;
+
+    public Car(CarType model) {
+        this.model = model;
+    }
+
+    protected abstract void construct();
+
+    public CarType getModel() {
+        return model;
+    }
+
+    public void setModel(CarType model) {
+        this.model = model;
+    }
+}
