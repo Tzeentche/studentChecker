269b0176e9c3f83fa6e896f493b918947d4997a3
Tzeentche <ilyasugako87@gmail.com>
2020-04-03 23:23:14 +0300

    Add class SecondUsage to theme Lambdas.

---
+++ ITAcademyJAVA/src/by/sugako/lesson20/second/SecondUsage.java
@@ -0,0 +1,9 @@
+package by.sugako.lesson20.second;
+
+import java.util.function.IntBinaryOperator;
+
+public interface SecondUsage {
+
+//    public void printResult(IntBinaryOperator secondInterface, )
+    int printresult(int x, int y);
+}
