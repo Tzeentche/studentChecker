8d824b1ac1ba6b943a16ac240e8a4c14a75a9212
Tzeentche <ilyasugako87@gmail.com>
2020-04-03 23:22:49 +0300

    Add class Main to theme Lambdas.

---
+++ ITAcademyJAVA/src/by/sugako/lesson20/second/Main.java
@@ -0,0 +1,21 @@
+package by.sugako.lesson20.second;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        SecondUsage secndUsage = new SecondUsage();
+//        secondUsage.printResult(a, b) -> a + b, 1, 2);
+//        secondUsage.printResult(x1, x2) -> x1 * x2, 3, 4);
+//        secondUsage.printResult(val1, val2) -> 500, 4, 5);
+//        secondUsage.printResult(x, y) -> x/y, 200, 25);
+//
+//        secondUsage.printResult(min, max) -> {
+//           for(int i = min; i < max; i++) {
+//                System.out.println(i + " ");
+//            }
+//            return result;
+//        }, 3, 7);
+    }
+    public static int globalResult = -1;
+}
