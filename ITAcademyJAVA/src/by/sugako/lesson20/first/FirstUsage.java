33a524237326be2f5f4a06f72a22aeac9d76b7c0
Tzeentche <ilyasugako87@gmail.com>
2020-04-04 23:23:45 +0300

    Add class FirstUsage to lesson 20.

--- ITAcademyJAVA/src/by/sugako/lesson20/first/FirstUsage.java
+++ ITAcademyJAVA/src/by/sugako/lesson20/first/FirstUsage.java
@@ -8,5 +8,19 @@ public class FirstUsage {
         System.out.println(firstInterface.getAsDouble());
     }
 
+    public static void main(String[] args) {
+        new FirstUsage().printDouble(() -> 123.45);
 
+        DoubleSupplier firstInterface1 = new FirstInterfaceImpl();
+        new FirstUsage().printDouble(firstInterface1);
+    }
+
+}
+
+class FirstInterfaceImpl implements DoubleSupplier {
+
+    @Override
+    public double getAsDouble() {
+        return 123.45;
+    }
 }
