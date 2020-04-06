11679cfbcc0bd7a7e9e644c4ecc1f05627ab4360
Tzeentche <ilyasugako87@gmail.com>
2020-04-06 23:01:12 +0300

    Refactored class FirstExample to lesson 20.

--- ITAcademyJAVA/src/by/sugako/lesson20/stream/FirstExample.java
+++ ITAcademyJAVA/src/by/sugako/lesson20/stream/FirstExample.java
@@ -2,6 +2,7 @@ package by.sugako.lesson20.stream;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class FirstExample {
 
@@ -24,11 +25,42 @@ public class FirstExample {
                 .filter(s -> s != null)
                 .filter(s -> s.matches("[A-Za-z]"))
                 .forEach(System.out::println);
+
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(null);
+//        average(list1);
+        average2(null);
+        average2(list1);
     }
 
-    static void average(List<Integer> number) {
+    public static void average(List<Integer> numbers) {
 
-//        average - ?
+        if (numbers == null) return;
+        int sum = 0;
+        int count = 0;
 
+        for(Integer i:numbers) {
+            if(i != null) {
+                sum += i;
+                count++;
+            }
+        }
+
+        if(count != 0) {
+            System.out.println((double) sum / count);
+        }
+    }
+
+    public static void average2(List<Integer> numbers) {
+
+        if(numbers == null) return;
+        System.out.println(numbers.stream()
+                .filter(Objects::nonNull)
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(-1)
+        );
     }
 }
