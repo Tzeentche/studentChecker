b8efa1ace7c871e4f6ae415f5cf51035e6a39eb0
Tzeentche <ilyasugako87@gmail.com>
2020-04-08 23:32:47 +0300

    Add class EducationForm to Lesson 22.

---
+++ ITAcademyJAVA/src/by/sugako/lesson22/EducationForm.java
@@ -0,0 +1,18 @@
+package by.sugako.lesson22;
+
+public class EducationForm {
+
+    private String formName;
+
+    public EducationForm(String formName) {
+        this.formName = formName;
+    }
+
+    public String getFormName() {
+        return formName;
+    }
+
+    public void setFormName(String formName) {
+        this.formName = formName;
+    }
+}
