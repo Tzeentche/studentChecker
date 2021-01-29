cbecfb25c2f22c79a92e9c038e6478acd35470ab
Tzeentche <ilyasugako87@gmail.com>
2021-01-29 23:52:40 +0300

    Refactored StudentOrderDaoImplTests class.

--- Student_order/src/test/java/edu/javacourse/studentProj/dao/StudentOrderDaoImplTests.java
+++ Student_order/src/test/java/edu/javacourse/studentProj/dao/StudentOrderDaoImplTests.java
@@ -3,39 +3,32 @@ package edu.javacourse.studentProj.dao;
 import edu.javacourse.studentProj.domain.*;
 import edu.javacourse.studentProj.exception.DaoException;
 import org.junit.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.net.URL;
+import java.io.IOException;
+import java.net.*;
 import java.nio.file.*;
 import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 class DictionaryDaoImplTests {
 
 
+
+    private static final Logger logger = LoggerFactory.getLogger(DictionaryDaoImplTests.class);
     @BeforeClass
     public static void startUp() throws Exception {
-        URL url1 = DictionaryDaoImplTests.class.getClassLoader()
-                .getResource("student_project.sql");
-        URL url2 = DictionaryDaoImplTests.class.getClassLoader()
-                .getResource("student_data.sql");
-
-        List<String> str1 = Files.readAllLines(Paths.get(url1.toURI()));
-        String sql1 = str1.stream().collect((Collectors.joining()));
-
-        List<String> str2 = Files.readAllLines(Paths.get(url2.toURI()));
-        String sql2 = str2.stream().collect((Collectors.joining()));
-
-        try (Connection con = ConnectionBuilder.getConnection();
-             Statement stmt1 = con.createStatement();) {
-
-            stmt1.executeUpdate(sql1);
-            stmt1.executeUpdate(sql2);
-        }
+        DBInit.startUp();
     }
 
     @Test
     public void testStreet() throws DaoException {
+        LocalDateTime dt1 = LocalDateTime.now();
+        LocalDateTime dt2 = LocalDateTime.now();
+        logger.info("TEST {}, {} ", dt1, dt2);
         List<Street> d = new DictionaryDaoImpl().findStreets("про");
         Assert.assertTrue(d.size() == 3);
     }
@@ -62,8 +55,6 @@ class DictionaryDaoImplTests {
         Assert.assertTrue(ca3.size() == 2);
         List<CountryArea> ca4 = new DictionaryDaoImpl().findAreas("020010010000");
         Assert.assertTrue(ca4.size() == 2);
-
-
     }
 
     @Before
@@ -87,5 +78,4 @@ class DictionaryDaoImplTests {
         System.out.println("TEST 3");
         throw new RuntimeException("Bad Result");
     }
-
 }
