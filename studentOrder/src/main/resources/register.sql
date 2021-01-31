22ff8e4ed5b709bd2509bc4f6fbf0adb174b5342
Tzeentche <ilyasugako87@gmail.com>
2021-01-31 20:19:54 +0300

    Refactored register.sql file.

--- studentOrder/src/main/resources/register.sql
+++ studentOrder/src/main/resources/register.sql
@@ -10,11 +10,17 @@ CREATE TABLE cr_district (
     PRIMARY KEY (district_code)
 );
 
+INSERT INTO cr_district (district_code, district_name)
+VALUES (1, 'Выборгский');
+
 CREATE TABLE cr_street (
     street_code integer not null,
     street_name text,
     PRIMARY KEY (street_code)
 );
+
+INSERT INTO cr_street (street_code, street_name)
+VALUES (1, 'Самсоньевский проспект');
 
 CREATE TABLE cr_address (
     address_id SERIAL,
@@ -27,6 +33,9 @@ CREATE TABLE cr_address (
     FOREIGN KEY (disrict_code) REFERENCES cr_district(district_code) ON UPDATE NO ACTION ON DELETE RESTRICT,
     FOREIGN KEY (street_code) REFERENCES cr_streets (street_code) ON UPDATE NO ACTION ON DELETE RESTRICT
 );
+
+INSERT INTO cr_address (district_code, street_code, building, extension, apartment)
+VALUES (1, 1, '10', '2', '121');
 
 CREATE TABLE cr_person (
     person_id SERIAL,
@@ -42,6 +51,23 @@ CREATE TABLE cr_person (
     PRIMARY KEY (person_id)
 )
 
+INSERT INTO cr_person (sur_name, given_name, patronymic, date_of_birth, passport_seria, passport_number, passport_date,
+certficate_number, certificate_date)
+VALUES ('Васильев', 'Павел','Николаевич','1995-03-18','1234','123456', '2015-04-11', null, null);
+
+INSERT INTO cr_person (sur_name, given_name, patronymic, date_of_birth, passport_seria, passport_number, passport_date,
+certficate_number, certificate_date)
+VALUES ('Васильева', 'Ирина','Петровна','1997-08-21','4321','654321', '2017-09-19', null, null);
+
+INSERT INTO cr_person (sur_name, given_name, patronymic, date_of_birth, passport_seria, passport_number, passport_date,
+certficate_number, certificate_date)
+VALUES ('Васильев', 'Евгения','Павловна','2016-01-11', null, null, null, '456123', '2016-01-21');
+
+INSERT INTO cr_person (sur_name, given_name, patronymic, date_of_birth, passport_seria, passport_number, passport_date,
+certficate_number, certificate_date)
+VALUES ('Васильев', 'Александр','Павлович','2018-10-24', null, null, null, '321654', '2018-11-09');
+
+
 CREATE TABLE cr_adress_person (
     person_address_id SERIAL,
     address_id integer not null,
@@ -51,7 +77,16 @@ CREATE TABLE cr_adress_person (
     PRIMARY KEY (person_address_id),
     FOREIGN KEY (address_id) REFERENCES cr_address(address_id) ON UPDATE NO ACTION ON DELETE RESTRICT,
     FOREIGN KEY (person_id) REFERENCES cr_person(person_id) ON UPDATE NO ACTION ON DELETE RESTRICT
+);
 
-)
+INSERT INTO cr_address_person (address_id, person_id, start_date, end_date)
+VALUES (1, 1, '2014-10-12', null);
 
+INSERT INTO cr_address_person (address_id, person_id, start_date, end_date)
+VALUES (1, 2, '2014-10-12', null);
 
+INSERT INTO cr_address_person (address_id, person_id, start_date, end_date)
+VALUES (1, 3, '2016-02-05', null);
+
+INSERT INTO cr_address_person (address_id, person_id, start_date, end_date)
+VALUES (1, 3, '2018-12-11', null);
