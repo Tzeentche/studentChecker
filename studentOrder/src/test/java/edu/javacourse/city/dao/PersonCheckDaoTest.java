0b6e9f266f56b0ded3341e315b3141197b6bcee8
Tzeentche <ilyasugako87@gmail.com>
2021-02-02 22:55:02 +0300

    Refactored PersonCheckDaoTest class.

--- studentOrder/src/test/java/edu/javacourse/city/dao/PersonCheckDaoTest.java
+++ studentOrder/src/test/java/edu/javacourse/city/dao/PersonCheckDaoTest.java
@@ -1,12 +1,34 @@
 package edu.javacourse.city.dao;
 
+import edu.javacourse.city.domain.PersonRequest;
+import edu.javacourse.city.domain.PersonResponse;
+import edu.javacourse.city.exception.PersonCheckException;
+import edu.javacourse.studentProj.domain.Person;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.time.LocalDate;
 
 import static org.junit.Assert.*;
 
 public class PersonCheckDaoTest {
 
     @Test
-    public void checkPerson() {
+    public void checkPerson() throws PersonCheckException {
+
+        PersonRequest pr = new PersonRequest();
+        pr.setSurName("Васильев");
+        pr.setGivenName("Павел");
+        pr.setPatronymic("Николаевич");
+        pr.setDateOfBirth(LocalDate.of(1995, 3, 18));
+        pr.setStreetCode(1);
+        pr.setBuilding("10");
+        pr.setExtension("2");
+        pr.setApartment("121");
+
+        PersonCheckDao dao = new PersonCheckDao();
+        PersonResponse ps = dao.checkPerson(pr);
+        Assert.assertTrue(ps.isRegistered());
+        Assert.assertTrue(ps.isTemporal());
     }
 }
