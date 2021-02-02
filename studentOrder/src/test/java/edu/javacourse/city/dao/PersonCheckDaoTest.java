7deb8b8ecfae2cf453f7bc947e0a6739b7e0823b
Tzeentche <ilyasugako87@gmail.com>
2021-02-02 22:47:21 +0300

    Create PersonCheckDaoTest class.

---
+++ studentOrder/src/test/java/edu/javacourse/city/dao/PersonCheckDaoTest.java
@@ -0,0 +1,12 @@
+package edu.javacourse.city.dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PersonCheckDaoTest {
+
+    @Test
+    public void checkPerson() {
+    }
+}
