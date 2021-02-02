ee3eab3375a54c4416a38e0eaa345b690b1ff198
Tzeentche <ilyasugako87@gmail.com>
2021-02-02 23:18:37 +0300

    Refactored PersonCheckDao class.

--- studentOrder/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
+++ studentOrder/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
@@ -8,7 +8,17 @@ import java.sql.*;
 
 public class PersonCheckDao {
 
-    private static final String SQL_REQUEST = "";
+    private static final String SQL_REQUEST = " select temporal from cr_address_person ap " +
+                    "inner join cr_person p on p.person_id = ap.person_id " +
+                    "inner join cr_address a on a.address_id = ap.address_id " +
+                    "where " +
+                    "CURRENT_DATE >= ap.start_date and (CURRENT_DATE <= ap.end_date or ap.end_date is null) " +
+                    "and upper(p.sur_name COLLATE \"en_US.UTF-8\") = upper(? COLLATE \"en_US.UTF-8\")  " +
+                    "and upper(p.given_name COLLATE \"en_US.UTF-8\") = upper(? COLLATE \"en_US.UTF-8\")  " +
+                    "and upper(patronymic COLLATE \"en_US.UTF-8\") = upper(? COLLATE \"en_US.UTF-8\")  " +
+                    "and p.date_of_birth = ? " +
+                    "and a.street_code = ?  " +
+                    "and upper(a.building COLLATE \"en_US.UTF-8\") = upper(? COLLATE \"en_US.UTF-8\")";
 
     public PersonResponse checkPerson(PersonRequest request) throws PersonCheckException {
         PersonResponse response = new PersonResponse();
