bb065239094709690d126dde294f5714f774045c
Tzeentche <ilyasugako87@gmail.com>
2021-02-03 23:17:44 +0300

    Refactored Server class.

--- studentOrder/src/test/java/edu/javacourse/net/Server.java
+++ studentOrder/src/test/java/edu/javacourse/net/Server.java
@@ -1,4 +1,36 @@
 package edu.javacourse.net;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket socket = new ServerSocket(25225);
+
+        while(true) {
+            Socket client = socket.accept();
+            handleRequest(client);
+        }
+    }
+
+    private static void handleRequest(Socket client) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+
+        StringBuilder sb = new StringBuilder("Hello, ");
+        String userName = br.readLine();
+
+        sb.append(userName);
+        bw.write(sb.toString());
+        bw.newLine();
+        bw.flush();
+
+        br.close();
+        bw.close();
+        client.close();
+    }
 }
