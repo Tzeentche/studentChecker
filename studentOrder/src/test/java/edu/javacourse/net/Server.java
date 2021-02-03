9d307318ba163a9228b73b1a1e1f9907a058880e
Tzeentche <ilyasugako87@gmail.com>
2021-02-03 23:52:57 +0300

    Refactored Server class.

--- studentOrder/src/test/java/edu/javacourse/net/Server.java
+++ studentOrder/src/test/java/edu/javacourse/net/Server.java
@@ -6,33 +6,55 @@ import java.net.Socket;
 
 public class Server {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         ServerSocket socket = new ServerSocket(25225);
 
         System.out.println("Server is started");
         while(true) {
             Socket client = socket.accept();
-            handleRequest(client);
+           new SimpleServer(client).start();
         }
     }
 
-    private static void handleRequest(Socket client) throws IOException {
+}
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+class SimpleServer extends Thread {
 
-        StringBuilder sb = new StringBuilder("Hello, ");
-        String userName = br.readLine();
-        System.out.println("Server got dtring: " + userName);
+    private Socket client;
 
-        sb.append(userName);
-        bw.write(sb.toString());
-        bw.newLine();
-        bw.flush();
+    public SimpleServer(Socket client) {
 
-        br.close();
-        bw.close();
-        client.close();
+        this.client = client;
+    }
+    public void run() {
+
+        handledRequest();
+    }
+
+    private void handledRequest() {
+
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+
+            StringBuilder sb = new StringBuilder("Hello, ");
+            String userName = br.readLine();
+            System.out.println("Server got dtring: " + userName);
+            Thread.sleep(2000);
+
+            sb.append(userName);
+            bw.write(sb.toString());
+            bw.newLine();
+            bw.flush();
+
+            br.close();
+            bw.close();
+            client.close();
+
+        } catch(Exception ex) {
+
+            ex.printStackTrace(System.out);
+        }
     }
 }
