19fd97cc59a014bc807a9e4f2db18cf337a4be68
Tzeentche <ilyasugako87@gmail.com>
2021-02-10 23:09:47 +0300

    Refactored CheckPersonServlet class.

--- studentOrder/src/main/java/edu/javacourse/web/CheckPersonServlet.java
+++ studentOrder/src/main/java/edu/javacourse/web/CheckPersonServlet.java
@@ -3,6 +3,8 @@ package edu.javacourse.web;
 import edu.javacourse.city.dao.PersonCheckDao;
 import edu.javacourse.city.domain.PersonRequest;
 import edu.javacourse.city.domain.PersonResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,6 +16,17 @@ import java.time.LocalDate;
 
 @WebServlet(name = "CheckPersonServlet",  urlPatterns = {"/checkPerson"})
 public class CheckPersonServlet extends HttpServlet {
+
+    private static final Logger logger = LoggerFactory.getLogger(CheckPersonServlet.class);
+
+
+    private PersonCheckDao dao;
+
+    @Override
+    public void init() throws ServletException {
+        logger.info("SERVLET is created");
+        dao = new PersonCheckDao();
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
