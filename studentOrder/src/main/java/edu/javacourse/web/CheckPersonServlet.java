86005b22b32dcf85e66705812bb428eb71fda03b
Tzeentche <ilyasugako87@gmail.com>
2021-02-10 22:09:46 +0300

    Refactored CheckPersonServlet class.

--- studentOrder/src/main/java/edu/javacourse/web/CheckPersonServlet.java
+++ studentOrder/src/main/java/edu/javacourse/web/CheckPersonServlet.java
@@ -1,11 +1,13 @@
 package edu.javacourse.web;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(name = "CheckPersonServlet",  urlPatterns = {"/checkPerson"})
 public class CheckPersonServlet extends HttpServlet {
 
     @Override
