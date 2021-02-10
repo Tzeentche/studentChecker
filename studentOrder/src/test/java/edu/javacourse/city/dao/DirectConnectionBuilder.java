befe602c57059661a408a6a4f5f0389389e39f2f
Tzeentche <ilyasugako87@gmail.com>
2021-02-10 23:30:04 +0300

    Refactored DirectConnectionBuilder class.

--- studentOrder/src/test/java/edu/javacourse/city/dao/DirectConnectionBuilder.java
+++ studentOrder/src/test/java/edu/javacourse/city/dao/DirectConnectionBuilder.java
@@ -1,4 +1,12 @@
 package edu.javacourse.city.dao;
 
-public class DirectConnectionBuilder {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DirectConnectionBuilder implements ConnectionBuilder {
+    @Override
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:postgresql://localhost/city_register", "postgres", "postgres");
+    }
 }
