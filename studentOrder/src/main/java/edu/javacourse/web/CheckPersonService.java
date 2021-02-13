c4c64ba35c310bfcc7ddceb88c8798ad6758deb8
Tzeentche <ilyasugako87@gmail.com>
2021-02-13 23:39:50 +0300

    Refactored CheckPersonService class.

--- studentOrder/src/main/java/edu/javacourse/web/CheckPersonService.java
+++ studentOrder/src/main/java/edu/javacourse/web/CheckPersonService.java
@@ -1,9 +1,10 @@
 package edu.javacourse.web;
 
-import edu.javacourse.city.domain.PersonResponse;
+import edu.javacourse.city.domain.PersonRequest;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.time.LocalDate;
 
 @Path("/check")
 public class CheckPersonService {
@@ -11,9 +12,19 @@ public class CheckPersonService {
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public PersonResponse checkPerson(@PathParam("id") int simpleId,
-                                      @QueryParam("name") String simpleName) {
-        return new PersonResponse();
+    public PersonRequest checkPerson(@PathParam("id") int simpleId,
+                                     @QueryParam("name") String simpleName) {
+
+        PersonRequest pr = new PersonRequest();
+        pr.setSurName("Васильева");
+        pr.setGivenName("Ирина");
+        pr.setPatronymic("Петровна");
+        pr.setDateOfBirth(LocalDate.of(1995, 3, 18));
+        pr.setStreetCode(1);
+        pr.setBuilding("271");
+        pr.setApartment("4");
+
+        return pr;
     }
 }
 
