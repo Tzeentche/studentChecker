d36c2637e912d9e7bf85a403b4069ba5d126e813
Tzeentche <ilyasugako87@gmail.com>
2021-02-15 22:59:38 +0300

    Refactored CheckPersonService class.

--- studentOrder/src/main/java/edu/javacourse/web/CheckPersonService.java
+++ studentOrder/src/main/java/edu/javacourse/web/CheckPersonService.java
@@ -1,39 +1,42 @@
 package edu.javacourse.web;
 
+import edu.javacourse.city.dao.PersonCheckDao;
+import edu.javacourse.city.dao.PoolConnectionBuilder;
 import edu.javacourse.city.domain.PersonRequest;
 import edu.javacourse.city.domain.PersonResponse;
+import edu.javacourse.city.exception.PersonCheckException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.ejb.Singleton;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.time.LocalDate;
 
 @Path("/check")
+@Singleton
 public class CheckPersonService {
 
     private static final Logger logger = LoggerFactory.getLogger(CheckPersonService.class);
 
+    private PersonCheckDao dao;
     @PostConstruct
     public void init() {
-        logger.info("START");
-    }
-
-    @PreDestroy
-    public void destroy() {
-        logger.info("FINISH");
+        logger.info("SERVLET is created");
+        dao = new PersonCheckDao();
+        dao.setConnectionBuilder(new PoolConnectionBuilder());
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public PersonResponse checkPerson(PersonRequest request) {
+    public PersonResponse checkPerson(PersonRequest request) throws PersonCheckException {
 
-        System.out.println(request.toString());
+        logger.info(request.toString());
 
-        return new PersonResponse();
+        return dao.checkPerson(request);
     }
 }
 
