4b11c966132d45bcad0e44270a4b2066fa92ed5b
Tzeentche <ilyasugako87@gmail.com>
2021-02-15 23:23:50 +0300

    Create LocalDateAdapter class.

--- studentOrder/src/main/java/edu/javacourse/city/domain/LocalDateAdapter.java
+++ studentOrder/src/main/java/edu/javacourse/city/domain/LocalDateAdapter.java
@@ -1,4 +1,10 @@
 package edu.javacourse.city.domain;
 
-public class LocalDateAdapter {
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateAdapter extends XmlJavaTypeAdapter<String, LocalDate> {
+
+   
 }
