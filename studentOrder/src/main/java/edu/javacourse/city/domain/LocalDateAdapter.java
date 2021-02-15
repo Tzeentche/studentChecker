e5d7a71c31876c8e8df51ae8cf35b97c9d7544fd
Tzeentche <ilyasugako87@gmail.com>
2021-02-15 23:24:03 +0300

    Refactored LocalDateAdapter class.

--- studentOrder/src/main/java/edu/javacourse/city/domain/LocalDateAdapter.java
+++ studentOrder/src/main/java/edu/javacourse/city/domain/LocalDateAdapter.java
@@ -6,5 +6,15 @@ import java.time.format.DateTimeFormatter;
 
 public class LocalDateAdapter extends XmlJavaTypeAdapter<String, LocalDate> {
 
-   
+    public static final String PATTERN = "dd.MM.yyyy";
+
+    @Override
+    public LocalDate unmarshal(String v) throws Exception {
+        return LocalDate.parse(v, DateTimeFormatter.ofPattern(PATTERN));
+    }
+
+    @Override
+    public String marshal(LocalDate v) throws Exception {
+        return v.format(DateTimeFormatter.ofPattern(PATTERN));
+    }
 }
