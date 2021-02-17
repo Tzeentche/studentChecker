327dcfdd691d7aa75df6e807a143209abf8384ad
Tzeentche <ilyasugako87@gmail.com>
2021-02-17 23:25:18 +0300

    Refactored Person class.

--- studentOrder/src/test/java/edu/javacourse/city/domain/register/Person.java
+++ studentOrder/src/test/java/edu/javacourse/city/domain/register/Person.java
@@ -1,4 +1,41 @@
 package edu.javacourse.city.domain.register;
 
+import javax.persistence.*;
+
+@Entity
+@Table (name = "person")
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "person_id")
+    private long personId;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+
+    public long getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(long personId) {
+        this.personId = personId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
