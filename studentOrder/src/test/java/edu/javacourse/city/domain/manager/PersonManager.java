53465a82212116397eaa32e9b8fe844a210e6c48
Tzeentche <ilyasugako87@gmail.com>
2021-02-17 23:40:50 +0300

    Refactored PersonManager class.

--- studentOrder/src/test/java/edu/javacourse/city/domain/manager/PersonManager.java
+++ studentOrder/src/test/java/edu/javacourse/city/domain/manager/PersonManager.java
@@ -1,4 +1,35 @@
 package edu.javacourse.city.domain.manager;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import javax.imageio.spi.ServiceRegistry;
+
 public class PersonManager {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    private static SessionFactory buildSessionFactory() {
+
+        try {
+
+            StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .configure("hibernate_cfg.xml").build();
+
+            Metadata metadata = new MetadataSources(serviceRegistry).getMetadataBuilder().build();
+
+            return metadata.getSessionFactoryBuilder().build();
+
+        } catch(Exception ex) {
+
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
 }
