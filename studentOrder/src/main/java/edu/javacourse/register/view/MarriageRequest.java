c07413417f1da920e0eef6d2a2e33eb6474912d9
Tzeentche <ilyasugako87@gmail.com>
2021-02-19 23:31:27 +0300

    Refactored MarriageRequest class.

--- studentOrder/src/main/java/edu/javacourse/register/view/MarriageRequest.java
+++ studentOrder/src/main/java/edu/javacourse/register/view/MarriageRequest.java
@@ -1,4 +1,25 @@
 package edu.javacourse.register.view;
 
-public class MarriageRequest {
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class MarriageRequest implements Serializable {
+
+    private String husbandSurname;
+    private String husbandGivenName;
+    private String husbandPatronymic;
+    private LocalDate husbandDateBirth;
+    private String husbandPassportSeria;
+    private String husbandPassportNumber;
+    private LocalDate husbandPassportIssueDate;
+    private String wifeSurname;
+    private String wifeGivenName;
+    private String wifePatronymic;
+    private LocalDate wifeDateBirth;
+    private String wifePassportSeria;
+    private String wifePassportNumber;
+    private LocalDate wifePassportIssueDate;
+
+    private String marriageSertificateNumber;
+    private LocalDate marriageSertificateDate;
 }
