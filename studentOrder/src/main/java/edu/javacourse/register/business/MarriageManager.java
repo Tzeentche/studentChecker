2e50f44ecf4dd76edc683e15bb328c678aae9be6
Tzeentche <ilyasugako87@gmail.com>
2021-02-19 23:40:58 +0300

    Refactored MarriageManager class.

--- studentOrder/src/main/java/edu/javacourse/register/business/MarriageManager.java
+++ studentOrder/src/main/java/edu/javacourse/register/business/MarriageManager.java
@@ -1,9 +1,12 @@
 package edu.javacourse.register.business;
 
+import edu.javacourse.register.dao.MarriageDao;
 import edu.javacourse.register.view.MarriageRequest;
 import edu.javacourse.register.view.MarriageResponse;
 
 public class MarriageManager {
+
+    private MarriageDao dao;
 
     public MarriageResponse findMarriageCertificate(MarriageRequest request) {
 
