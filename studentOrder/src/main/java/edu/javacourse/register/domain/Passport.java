0207957668d554355866fac24be073921ec71517
Tzeentche <ilyasugako87@gmail.com>
2021-02-20 23:31:18 +0300

    Refactored Passport class.

--- studentOrder/src/main/java/edu/javacourse/register/domain/Passport.java
+++ studentOrder/src/main/java/edu/javacourse/register/domain/Passport.java
@@ -1,6 +1,52 @@
 package edu.javacourse.register.domain;
 
+import java.time.LocalDate;
+
 public class Passport {
 
-    
+    private Long PassportId;
+    private String serial;
+    private String number;
+    private LocalDate issueDate;
+    private String issueDepartment;
+
+    public Long getPassportId() {
+        return PassportId;
+    }
+
+    public void setPassportId(Long passportId) {
+        PassportId = passportId;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public LocalDate getIssueDate() {
+        return issueDate;
+    }
+
+    public void setIssueDate(LocalDate issueDate) {
+        this.issueDate = issueDate;
+    }
+
+    public String getIssueDepartment() {
+        return issueDepartment;
+    }
+
+    public void setIssueDepartment(String issueDepartment) {
+        this.issueDepartment = issueDepartment;
+    }
 }
