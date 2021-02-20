b36b50b9a6b30ae6d07ff061686b17b46e60fd05
Tzeentche <ilyasugako87@gmail.com>
2021-02-20 23:31:04 +0300

    Refactored Person class.

--- studentOrder/src/main/java/edu/javacourse/register/domain/Person.java
+++ studentOrder/src/main/java/edu/javacourse/register/domain/Person.java
@@ -1,6 +1,7 @@
 package edu.javacourse.register.domain;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class Person {
 
@@ -9,4 +10,53 @@ public class Person {
     private String lastName;
     private String patronimyc;
     private LocalDate dateOfBirth;
+    private List<Passport> passports;
+
+    public Long getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Long personId) {
+        this.personId = personId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPatronimyc() {
+        return patronimyc;
+    }
+
+    public void setPatronimyc(String patronimyc) {
+        this.patronimyc = patronimyc;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public List<Passport> getPassports() {
+        return passports;
+    }
+
+    public void setPassports(List<Passport> passports) {
+        this.passports = passports;
+    }
 }
