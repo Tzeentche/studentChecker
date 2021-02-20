f89bd521dcce428cfc8b313dfa50b07d564162eb
Tzeentche <ilyasugako87@gmail.com>
2021-02-20 23:24:36 +0300

    Refactored Person class.

--- studentOrder/src/main/java/edu/javacourse/register/domain/Person.java
+++ studentOrder/src/main/java/edu/javacourse/register/domain/Person.java
@@ -1,4 +1,12 @@
 package edu.javacourse.register.domain;
 
+import java.time.LocalDate;
+
 public class Person {
+
+    private Long personId;
+    private String firstName;
+    private String lastName;
+    private String patronimyc;
+    private LocalDate dateOfBirth;
 }
