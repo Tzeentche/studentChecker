1175500438c72e6823d71647106e01742a26a0c0
Tzeentche <ilyasugako87@gmail.com>
2021-02-21 22:40:06 +0300

    Create PersonDaoTest class.

---
+++ studentOrder/src/test/java/edu/javacourse/register/dao/PersonDaoTest.java
@@ -0,0 +1,12 @@
+package edu.javacourse.register.dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PersonDaoTest {
+
+    @Test
+    public void findPersons() {
+    }
+}
