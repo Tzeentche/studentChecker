4e632fe905aa4606728cc82b0793b34e6b0ffdf3
Tzeentche <ilyasugako87@gmail.com>
2021-02-21 22:33:24 +0300

    Refactored register_office.sql file.

--- studentOrder/src/main/resources/META-INF/register_office.sql
+++ studentOrder/src/main/resources/META-INF/register_office.sql
@@ -1,8 +1,9 @@
-DROP TABLE if EXISTS person;
-DROP TABLE if EXISTS passport;
+DROP TABLE if EXISTS ro_person;
+DROP TABLE if EXISTS ro_passport;
 
 CREATE TABLE ro_person (
     person_id SERIAL,
+    sex smallInt not null,
     first_name varchar(100),
     last_name varchar(100),
     patronymic varchar(100),
@@ -20,3 +21,7 @@ CREATE TABLE ro_passport (
     PRIMARY KEY (passport_id)
     FOREIGN KEY (person_id) REFERENCES ro_person(person_id) ON DELETE RESTRICT
 );
+
+INSERT IN ro_person (sex, first_name, last_name, patronymic, date_birth)
+VALUES (1, 'Елена', 'Васильева', 'Сергеевна', 1998-03-24),
+VALUES (2, 'Олег', 'Васильев', 'Петрович', 1997-10-16);
