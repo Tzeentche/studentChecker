8ea5a230e128a72937d45ad7df383a827065e6d6
Tzeentche <ilyasugako87@gmail.com>
2021-02-21 22:50:34 +0300

    Refactored PersonFemale class.

--- studentOrder/src/main/java/edu/javacourse/register/domain/PersonFemale.java
+++ studentOrder/src/main/java/edu/javacourse/register/domain/PersonFemale.java
@@ -1,9 +1,11 @@
 package edu.javacourse.register.domain;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import java.util.List;
 
 @Entity
+@DiscriminatorValue("1")
 public class PersonFemale extends Person {
 
     private List<MarriageCertificate> marriageCertificates;
