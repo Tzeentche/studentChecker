c6a496256e8cdf54c815c13e07523a8352ef3d75
Tzeentche <ilyasugako87@gmail.com>
2021-02-23 23:47:22 +0300

    Refactored MarriageDao class.

--- studentOrder/src/main/java/edu/javacourse/register/dao/MarriageDao.java
+++ studentOrder/src/main/java/edu/javacourse/register/dao/MarriageDao.java
@@ -1,6 +1,21 @@
 package edu.javacourse.register.dao;
 
+import edu.javacourse.register.domain.MarriageCertificate;
+import edu.javacourse.register.view.MarriageRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.persistence.EntityManager;
+
 public class MarriageDao {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MarriageDao.class);
 
+    private EntityManager entityManager;
+
+    public MarriageCertificate findMarriageCertificate(MarriageRequest request) {
+
+        LOGGER.info("findMarriageCertificate called");
+        return null;
+    }
 }
