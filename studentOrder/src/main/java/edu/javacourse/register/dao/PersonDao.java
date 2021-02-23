a269cdef3b0a65c3f8473f6469094897aa2447f0
Tzeentche <ilyasugako87@gmail.com>
2021-02-23 23:24:58 +0300

    Refactored PersonDao class.

--- studentOrder/src/main/java/edu/javacourse/register/dao/PersonDao.java
+++ studentOrder/src/main/java/edu/javacourse/register/dao/PersonDao.java
@@ -5,6 +5,7 @@ import edu.javacourse.register.domain.Person;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 import java.util.List;
 
 public class PersonDao {
@@ -19,6 +20,9 @@ public class PersonDao {
 
     public List<Person> findPersons() {
 
-        return entityManager.createQuery("SELECT p FROM PERSON p ").getResultList();
+        Query query = entityManager.createNamedQuery("Person.findPersons");
+        query.setParameter("personId", 1L);
+
+        return query.getResultList();
     }
 }
