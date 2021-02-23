0b25ba0cef6f891d80a8aec0f62f9360e00f3ce9
Tzeentche <ilyasugako87@gmail.com>
2021-02-23 23:47:35 +0300

    Refactored MarriageManager class.

--- studentOrder/src/main/java/edu/javacourse/register/business/MarriageManager.java
+++ studentOrder/src/main/java/edu/javacourse/register/business/MarriageManager.java
@@ -1,16 +1,24 @@
 package edu.javacourse.register.business;
 
 import edu.javacourse.register.dao.MarriageDao;
+import edu.javacourse.register.domain.MarriageCertificate;
+import edu.javacourse.register.rest.MarriageController;
 import edu.javacourse.register.view.MarriageRequest;
 import edu.javacourse.register.view.MarriageResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MarriageManager {
 
-    private MarriageDao dao;
+    private static final Logger LOGGER = LoggerFactory.getLogger(MarriageManager.class);
+
+    private MarriageDao marriageDao;
 
     public MarriageResponse findMarriageCertificate(MarriageRequest request) {
+        LOGGER.info("findMarriageCertificate called");
+        MarriageCertificate cert = marriageDao.findMarriageCertificate(request);
 
-        throw new UnsupportedOperationException("Unsupported");
+        return new MarriageResponse();
     }
 
 }
