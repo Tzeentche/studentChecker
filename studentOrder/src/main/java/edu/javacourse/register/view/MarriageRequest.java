b012e7769fa9c8496afec6a958227740acd4a8fa
Tzeentche <ilyasugako87@gmail.com>
2021-02-23 23:28:24 +0300

    Refactored MarriageRequest class.

--- studentOrder/src/main/java/edu/javacourse/register/view/MarriageRequest.java
+++ studentOrder/src/main/java/edu/javacourse/register/view/MarriageRequest.java
@@ -22,4 +22,132 @@ public class MarriageRequest implements Serializable {
 
     private String marriageSertificateNumber;
     private LocalDate marriageSertificateDate;
+
+    public String getHusbandSurname() {
+        return husbandSurname;
+    }
+
+    public void setHusbandSurname(String husbandSurname) {
+        this.husbandSurname = husbandSurname;
+    }
+
+    public String getHusbandGivenName() {
+        return husbandGivenName;
+    }
+
+    public void setHusbandGivenName(String husbandGivenName) {
+        this.husbandGivenName = husbandGivenName;
+    }
+
+    public String getHusbandPatronymic() {
+        return husbandPatronymic;
+    }
+
+    public void setHusbandPatronymic(String husbandPatronymic) {
+        this.husbandPatronymic = husbandPatronymic;
+    }
+
+    public LocalDate getHusbandDateBirth() {
+        return husbandDateBirth;
+    }
+
+    public void setHusbandDateBirth(LocalDate husbandDateBirth) {
+        this.husbandDateBirth = husbandDateBirth;
+    }
+
+    public String getHusbandPassportSeria() {
+        return husbandPassportSeria;
+    }
+
+    public void setHusbandPassportSeria(String husbandPassportSeria) {
+        this.husbandPassportSeria = husbandPassportSeria;
+    }
+
+    public String getHusbandPassportNumber() {
+        return husbandPassportNumber;
+    }
+
+    public void setHusbandPassportNumber(String husbandPassportNumber) {
+        this.husbandPassportNumber = husbandPassportNumber;
+    }
+
+    public LocalDate getHusbandPassportIssueDate() {
+        return husbandPassportIssueDate;
+    }
+
+    public void setHusbandPassportIssueDate(LocalDate husbandPassportIssueDate) {
+        this.husbandPassportIssueDate = husbandPassportIssueDate;
+    }
+
+    public String getWifeSurname() {
+        return wifeSurname;
+    }
+
+    public void setWifeSurname(String wifeSurname) {
+        this.wifeSurname = wifeSurname;
+    }
+
+    public String getWifeGivenName() {
+        return wifeGivenName;
+    }
+
+    public void setWifeGivenName(String wifeGivenName) {
+        this.wifeGivenName = wifeGivenName;
+    }
+
+    public String getWifePatronymic() {
+        return wifePatronymic;
+    }
+
+    public void setWifePatronymic(String wifePatronymic) {
+        this.wifePatronymic = wifePatronymic;
+    }
+
+    public LocalDate getWifeDateBirth() {
+        return wifeDateBirth;
+    }
+
+    public void setWifeDateBirth(LocalDate wifeDateBirth) {
+        this.wifeDateBirth = wifeDateBirth;
+    }
+
+    public String getWifePassportSeria() {
+        return wifePassportSeria;
+    }
+
+    public void setWifePassportSeria(String wifePassportSeria) {
+        this.wifePassportSeria = wifePassportSeria;
+    }
+
+    public String getWifePassportNumber() {
+        return wifePassportNumber;
+    }
+
+    public void setWifePassportNumber(String wifePassportNumber) {
+        this.wifePassportNumber = wifePassportNumber;
+    }
+
+    public LocalDate getWifePassportIssueDate() {
+        return wifePassportIssueDate;
+    }
+
+    public void setWifePassportIssueDate(LocalDate wifePassportIssueDate) {
+        this.wifePassportIssueDate = wifePassportIssueDate;
+    }
+
+    public String getMarriageSertificateNumber() {
+        return marriageSertificateNumber;
+    }
+
+    public void setMarriageSertificateNumber(String marriageSertificateNumber) {
+        this.marriageSertificateNumber = marriageSertificateNumber;
+    }
+
+    public LocalDate getMarriageSertificateDate() {
+        return marriageSertificateDate;
+    }
+
+    public void setMarriageSertificateDate(LocalDate marriageSertificateDate) {
+        this.marriageSertificateDate = marriageSertificateDate;
+    }
 }
