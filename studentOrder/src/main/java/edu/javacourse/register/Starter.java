066b93384f7a61dddebe0a592e0f0ecff28c8a86
Tzeentche <ilyasugako87@gmail.com>
2021-02-24 23:32:14 +0300

    Refactored Starter class.

--- studentOrder/src/main/java/edu/javacourse/register/Starter.java
+++ studentOrder/src/main/java/edu/javacourse/register/Starter.java
@@ -13,7 +13,8 @@ public class Starter {
                 new String[]{"springContext.xml"}
         );
 
-        MarriageController controller = context.getBean("controller", MarriageController.class);
+        MarriageController controller = context.getBean(MarriageController.class);
+//        MarriageController controller = context.getBean("controller", MarriageController.class);
         controller.findMarriageCertificate(new MarriageRequest());
     }
 }
