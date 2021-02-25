5f698be40ef6c756aeea6fa0aad3633c0572c8fe
Tzeentche <ilyasugako87@gmail.com>
2021-02-25 23:13:10 +0300

    Refactored MarriageManager class.

--- studentOrder/src/main/java/edu/javacourse/register/business/MarriageManager.java
+++ studentOrder/src/main/java/edu/javacourse/register/business/MarriageManager.java
@@ -9,9 +9,12 @@ import edu.javacourse.register.view.MarriageResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service("marriageService")
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class MarriageManager {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MarriageManager.class);
@@ -21,19 +24,11 @@ public class MarriageManager {
     @Autowired
     private PersonDao personDao;
 
-    @Autowired
-    public void setMarriageDao(MarriageDao marriageDao) {
-        this.marriageDao = marriageDao;
-    }
-
-    @Autowired
-    public MarriageManager(PersonDao personDao) {
-        this.personDao = personDao;
-    }
-
     public MarriageResponse findMarriageCertificate(MarriageRequest request) {
         LOGGER.info("findMarriageCertificate called");
         MarriageCertificate cert = marriageDao.findMarriageCertificate(request);
+
+        personDao.findPersons();
 
         return new MarriageResponse();
     }
