f8f1ba273df8b57798f058246995d91c50cd0df9
Tzeentche <ilyasugako87@gmail.com>
2021-02-27 23:31:31 +0300

    Refactored MarriageDao class.

--- studentOrder/src/main/java/edu/javacourse/register/dao/MarriageDao.java
+++ studentOrder/src/main/java/edu/javacourse/register/dao/MarriageDao.java
@@ -5,26 +5,28 @@ import edu.javacourse.register.view.MarriageRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 
-@Component
-public class MarriageDao {
+@Repository
+public interface MarriageDao extends JpaRepository<MarriageCertificate, Long> {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(MarriageDao.class);
-
-    private EntityManager entityManager;
-    @Value("${test.value}")
-    private String test;
-
-    public void setTest(String test) {
-        this.test = test;
-    }
-
-    public MarriageCertificate findMarriageCertificate(MarriageRequest request) {
-
-        LOGGER.info("findMarriageCertificate called:{}", test);
-        return null;
-    }
+//    private static final Logger LOGGER = LoggerFactory.getLogger(MarriageDao.class);
+//
+//    private EntityManager entityManager;
+//    @Value("${test.value}")
+//    private String test;
+//
+//    public void setTest(String test) {
+//        this.test = test;
+//    }
+//
+//    public MarriageCertificate findMarriageCertificate(MarriageRequest request) {
+//
+//        LOGGER.info("findMarriageCertificate called:{}", test);
+//        return null;
+//    }
 }
