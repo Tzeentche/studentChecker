83fc600d0cfbd2c26951c53f99642543f9dbb849
Tzeentche <ilyasugako87@gmail.com>
2021-02-28 00:17:19 +0300

    Refactored MarriageDao class.

--- studentOrder/src/main/java/edu/javacourse/register/dao/MarriageDao.java
+++ studentOrder/src/main/java/edu/javacourse/register/dao/MarriageDao.java
@@ -6,14 +6,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.ws.rs.QueryParam;
+import java.awt.*;
 
 @Repository
 public interface MarriageDao extends JpaRepository<MarriageCertificate, Long> {
 
+    List<MarriageCertificate> findByNumber(String number);
+    List<MarriageCertificate> findByNum(@Param("number") String number);
+
+    @Query("SELECT mc FROM MarriageCertificate mc WHERE mc.number = :number")
+    List<MarriageCertificate> findSomething(@Param("number") String number);
 //    private static final Logger LOGGER = LoggerFactory.getLogger(MarriageDao.class);
 //
 //    private EntityManager entityManager;
