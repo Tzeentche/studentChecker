27501335237d4f605426a86e22dedd311e506d24
Tzeentche <ilyasugako87@gmail.com>
2021-03-01 23:37:51 +0300

    Refactored University class.

--- student/src/main/java/edu/javacourse/student/domain/University.java
+++ student/src/main/java/edu/javacourse/student/domain/University.java
@@ -1,4 +1,15 @@
 package edu.javacourse.student.domain;
 
+import javax.persistence.*;
+
+@Table(name = "sr_university")
+@Entity
 public class University {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "university_Id")
+    private Long universityId;
+    @Column(name = "university_named")
+    private Long universityName;
 }
