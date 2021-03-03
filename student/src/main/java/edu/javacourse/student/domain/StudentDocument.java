f4596a4ff32624c97de12d612e03f7a5611872ae
Tzeentche <ilyasugako87@gmail.com>
2021-03-03 23:17:34 +0300

    Refactored StudentDocument class.

--- student/src/main/java/edu/javacourse/student/domain/StudentDocument.java
+++ student/src/main/java/edu/javacourse/student/domain/StudentDocument.java
@@ -22,6 +22,64 @@ public class StudentDocument {
     @ManyToOne (cascade = {CascadeType.REFRESH}, fetch = FetchType.LAZY)
     @JoinColumn(name = "faculty_id")
     private Faculty faculty;
+    @Column(name = "student_form")
+    @Enumerated
+    private StudentForm studentFrom;
 
+    public Long getDocumentId() {
+        StudentForm.DAY.ordinal();
+        return documentId;
+    }
 
+    public void setDocumentId(Long documentId) {
+        this.documentId = documentId;
+    }
+
+    public String getDocumntNumber() {
+        return documntNumber;
+    }
+
+    public void setDocumntNumber(String documntNumber) {
+        this.documntNumber = documntNumber;
+    }
+
+    public String getDocumntDate() {
+        return documntDate;
+    }
+
+    public void setDocumntDate(String documntDate) {
+        this.documntDate = documntDate;
+    }
+
+    public String getExpireDate() {
+        return expireDate;
+    }
+
+    public void setExpireDate(String expireDate) {
+        this.expireDate = expireDate;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Faculty getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(Faculty faculty) {
+        this.faculty = faculty;
+    }
+
+    public StudentForm getStudentFrom() {
+        return studentFrom;
+    }
+
+    public void setStudentFrom(StudentForm studentFrom) {
+        this.studentFrom = studentFrom;
+    }
 }
