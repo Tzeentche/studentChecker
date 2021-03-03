9c14d044c242a898bb202a7b7310490667e2128d
Tzeentche <ilyasugako87@gmail.com>
2021-03-03 23:33:00 +0300

    Refactored springContext.xml file.

--- student/src/main/java/edu/javacourse/student/StudentForm.java
+++ student/src/main/java/edu/javacourse/student/StudentForm.java
@@ -1,4 +1,4 @@
-package edu.javacourse.student.domain;
+package edu.javacourse.student;
 
 public enum StudentForm {
 
--- student/src/main/java/edu/javacourse/student/University.java
+++ student/src/main/java/edu/javacourse/student/University.java
@@ -1,4 +1,4 @@
-package edu.javacourse.student.domain;
+package edu.javacourse.student;
 
 import javax.persistence.*;
 import java.util.List;
