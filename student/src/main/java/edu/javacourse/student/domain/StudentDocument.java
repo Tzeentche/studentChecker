b529473d02cedd565d21353aed69c2f8053e8e7f
Tzeentche <ilyasugako87@gmail.com>
2021-03-03 23:14:08 +0300

    Refactored StudentDocument class.

--- student/src/main/java/edu/javacourse/student/domain/StudentDocument.java
+++ student/src/main/java/edu/javacourse/student/domain/StudentDocument.java
@@ -1,4 +1,27 @@
 package edu.javacourse.student.domain;
 
+import javax.persistence.*;
+
+@Table(name = "sr_student_document")
+@Entity
 public class StudentDocument {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "document_Id")
+    private Long documentId;
+    @Column(name = "document_number")
+    private String documntNumber;
+    @Column(name = "document_date")
+    private String documntDate;
+    @Column(name = "expire_date")
+    private String expireDate;
+    @ManyToOne(cascade = {CascadeType.REFRESH}, fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id")
+    private Student student;
+    @ManyToOne (cascade = {CascadeType.REFRESH}, fetch = FetchType.LAZY)
+    @JoinColumn(name = "faculty_id")
+    private Faculty faculty;
+
+
 }
