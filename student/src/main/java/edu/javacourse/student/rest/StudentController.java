c17dac6285ff26b6d0e6810b6ee4164793f11943
Tzeentche <ilyasugako87@gmail.com>
2021-03-04 23:43:57 +0300

    Refactored StudentController class.

--- student/src/main/java/edu/javacourse/student/rest/StudentController.java
+++ student/src/main/java/edu/javacourse/student/rest/StudentController.java
@@ -1,4 +1,10 @@
 package edu.javacourse.student.rest;
 
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.Path;
+
+@Component
+@Path("/student")
 public class StudentController {
 }
