2a93b8ce1d29161a02d7445a224e12c64bd995f1
Tzeentche <ilyasugako87@gmail.com>
2021-03-05 23:30:15 +0300

    Create LocalDateAdapter class.

--- student/src/main/java/edu/javacourse/student/StudentResponse.java
+++ student/src/main/java/edu/javacourse/student/StudentResponse.java
@@ -11,4 +11,52 @@ public class StudentResponse {
     private String facultyName;
     private String universityName;
     private String studentForm;
+
+    public String getDocumentNumber() {
+        return documentNumber;
+    }
+
+    public void setDocumentNumber(String documentNumber) {
+        this.documentNumber = documentNumber;
+    }
+
+    public LocalDate getDocumentDate() {
+        return documentDate;
+    }
+
+    public void setDocumentDate(LocalDate documentDate) {
+        this.documentDate = documentDate;
+    }
+
+    public LocalDate getExpireDate() {
+        return expireDate;
+    }
+
+    public void setExpireDate(LocalDate expireDate) {
+        this.expireDate = expireDate;
+    }
+
+    public String getFacultyName() {
+        return facultyName;
+    }
+
+    public void setFacultyName(String facultyName) {
+        this.facultyName = facultyName;
+    }
+
+    public String getUniversityName() {
+        return universityName;
+    }
+
+    public void setUniversityName(String universityName) {
+        this.universityName = universityName;
+    }
+
+    public String getStudentForm() {
+        return studentForm;
+    }
+
+    public void setStudentForm(String studentForm) {
+        this.studentForm = studentForm;
+    }
 }
