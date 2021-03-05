4bacf548df99f7cc77f8ecbaf122482a6d851794
Tzeentche <ilyasugako87@gmail.com>
2021-03-05 23:29:46 +0300

    Refactored StudentResponse class.

--- student/src/main/java/edu/javacourse/student/StudentResponse.java
+++ student/src/main/java/edu/javacourse/student/StudentResponse.java
@@ -1,4 +1,14 @@
 package edu.javacourse.student;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
 public class StudentResponse {
+
+    private String documentNumber;
+    private LocalDate documentDate;
+    private LocalDate expireDate;
+    private String facultyName;
+    private String universityName;
+    private String studentForm;
 }
