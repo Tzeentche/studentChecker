1658641eea40ee78977be44c6ef026f43520b095
Tzeentche <ilyasugako87@gmail.com>
2021-03-05 23:58:41 +0300

    Refactored StudentController class.

--- student/src/main/java/edu/javacourse/student/rest/StudentController.java
+++ student/src/main/java/edu/javacourse/student/rest/StudentController.java
@@ -1,10 +1,29 @@
 package edu.javacourse.student.rest;
 
+import edu.javacourse.student.StudentResponse;
+import edu.javacourse.student.service.StudentService;
+import edu.javacourse.student.view.StudentRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Component
 @Path("/student")
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<StudentResponse> getStudentInfo(StudentRequest request) {
+
+        return getStudentInfo(request);
+    }
 }
